261495624a93ee73d7182465f2f674ff6b246c8b
KovalchukDanil0 <105009706+KovalchukDanil0@users.noreply.github.com>
2025-04-24 23:41:10 +0200

    fixes

---
+++ app/lib/info.ts
@@ -0,0 +1,6 @@
+export const location = "Prague, Czechia";
+export const email = "danilkovalchuk0@gmail.com";
+export const phone = "+420 792 526 408";
+export const github = "https://github.com/KovalchukDanil0";
+export const linkedIn = "https://linkedin.com/in/danylo-kovalchuk";
+export const metaSite = " | Danylo Resume";
